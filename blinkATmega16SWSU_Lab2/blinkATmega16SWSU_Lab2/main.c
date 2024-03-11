fa10a2263854c076d99eb39960a6265e28347e8c
Dmitriy <slagterra2017@yandex.ru>
2024-03-11 18:59:10 +0300

    code for laba 2 is done

--- blinkATmega16SWSU_Lab2/blinkATmega16SWSU_Lab2/main.c
+++ blinkATmega16SWSU_Lab2/blinkATmega16SWSU_Lab2/main.c
@@ -13,15 +13,18 @@
 
 int main(void)
 {
-	DDRA = ((1 << PA0) | (1 << PA1)); // pin 39, 40 -> output
-	PORTA = ((1 << PA0) | (0 << PA1)); // pin 39, 40 -> high, low
-
+	// pin 39, 40 -> output
+	DDRA |= (1 << PA0); 
+	DDRA |= (1 << PA1); 
+	 // pin 39, 40 -> low, high
+	PORTA |= (1 << PA0);
+	
     while(1)
     {
 	 
-	 PORTA = (1 << PA1);
+	 PORTA |= (1 << PA1);
 	 _delay_ms(1000);
-	 PORTA = (0 << PA1);
+	 PORTA &= ~(1 << PA1);
 	 _delay_ms(1000);
     }
 }
